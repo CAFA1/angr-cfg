f81800198ca4a1528d9c0f929b43af626c2b2136
longlong <longlong_vm@dell_book.com>
2020-06-17 16:46:03 +0800

    new cfg

--- test/cfg.py
+++ test/cfg.py
@@ -1,7 +1,18 @@
 import angr
 from angrutils import *
-proj = angr.Project("./mywps", load_options={'auto_load_libs':False})
-main = proj.loader.main_object.get_symbol("main")
-start_state = proj.factory.blank_state(addr=main.rebased_addr)
-cfg = proj.analyses.CFGEmulated(fail_fast=True, starts=[main.rebased_addr], initial_state=start_state)
-plot_cfg(cfg, "mywps", asminst=True, remove_imports=True, remove_path_terminator=True)  
+# # load your project
+p = angr.Project('./mywps', load_options={'auto_load_libs': False})
+# # Generate a static CFG
+cfg = p.analyses.CFGFast()
+print("This is the graph:", cfg.graph)
+print("It has %d nodes and %d edges" % (len(cfg.graph.nodes()), len(cfg.graph.edges())))
+
+entry_node = cfg.model.get_any_node(p.entry)
+print("There were %d contexts for the entry block" % len(cfg.model.get_all_nodes(p.entry)))
+
+print("Predecessors of the entry point:", entry_node.predecessors)
+print("Successors of the entry point:", entry_node.successors)
+print("Successors (and type of jump) of the entry point:", [ jumpkind + " to " + hex(node.addr) for node,jumpkind in cfg.model.get_successors_and_jumpkind(entry_node) ])
+
+# generate png
+plot_cfg(cfg, "mywps", asminst=True, remove_imports=True, remove_path_terminator=True)
